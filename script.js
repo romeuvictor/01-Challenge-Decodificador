d33e81151bf8cbd1d83517ef6775e3cf50ebe166
Romeu Victor da Rocha <romeuvictor@gmail.com>
2023-05-30 22:33:02 -0300

    Adiciona o java-script do projeto

---
+++ script.js
@@ -0,0 +1,45 @@
+const encriptarMensagem = document.querySelector(".encriptar-mensagem");
+const mensagemCodificada = document.querySelector(".codificado");
+
+function criptografar() {
+    const textoEncriptado = encriptar(encriptarMensagem.value);
+    mensagemCodificada.value = textoEncriptado;
+    encriptarMensagem.value = "";
+}
+
+
+function encriptar(stringEncriptada) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+
+function desencriptarMsg() {
+    const textoDesencriptado = desencriptar(encriptarMensagem.value);
+    mensagemCodificada.value = textoDesencriptado;
+    encriptarMensagem.value = "";
+}
+
+
+function desencriptar(stringDesencriptada) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+}
